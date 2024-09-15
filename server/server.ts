0f858f5c4ec6a345db5a8e0fe8c127acea8c3193
take-cantik <take.cantik17@gmail.com>
2024-09-15 16:52:40 +0900

    :zap: socket ioのパスを作成

--- server/server.ts
+++ server/server.ts
@@ -1,10 +1,18 @@
 import express from 'express';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
 
 const app: express.Express = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, { path: '/socket' });
 const port = 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+io.on('connection', (socket) => {
+  console.info('connected!');
 });
 
 app.listen(port, () => {
