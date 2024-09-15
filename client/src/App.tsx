831324e4f818c19b2c192cbd3a9503ea13f1844b
take-cantik <take.cantik17@gmail.com>
2024-09-15 18:50:50 +0900

    :zap: チャットのページのファイルを分けた

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,51 +1,9 @@
 import './App.css'
-import { useEffect, useState } from 'react';
-import { socket } from './infra/socket';
-import { useForm } from 'react-hook-form';
-
-type MessageReq = {
-  message: string;
-}
 
 function App() {
-  const [isConnected, setConnected] = useState<boolean>(socket.connected);
-  const [messageList, setMessageList] = useState<string[]>([]);
-  const { register, getValues, handleSubmit, reset } = useForm<MessageReq>({
-    defaultValues: {
-      message: '',
-    },
-  });
-
-  const submit = () => {
-    console.log(getValues('message'));
-    socket.emit('sendMessage', getValues('message'));
-    reset();
-  };
-
-  useEffect(() => {
-    socket.on('connect', () => {
-      setConnected(socket.connected);
-    });
-    socket.on('chat', (message: string) => {
-      setMessageList((prev) => [...prev, message]);
-    });
-  }, []);
-
   return (
     <div>
-      {isConnected ? 'Connected' : 'Disconnected'}
-      <form onSubmit={handleSubmit(submit)}>
-        <label>
-          メッセージ
-          <input {...register('message', { required: '必須項目です。' })} placeholder="メッセージを入力" />
-        </label>
-        <button type="submit">送信</button>
-      </form>
-      <ul>
-        {messageList.map((message, index) => (
-          <li key={index}>{message}</li>
-        ))}
-      </ul>
+      <h1>Hello</h1>
     </div>
   )
 }
