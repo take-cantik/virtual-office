954a0276178bb85d649a7ad6843f805f914d98f8
take-cantik <take.cantik17@gmail.com>
2024-09-15 17:30:37 +0900

    :sparkles: テストで通信してみた

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,34 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { io } from 'socket.io-client'
 import './App.css'
+import { useEffect, useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const socket = io('http://localhost:3000', {
+    withCredentials: true,
+    path: '/socket'
+  });
+
+  const [isConnected, setConnected] = useState<boolean>(socket.connected);
+
+  useEffect(() => {
+    socket.on('connect', () => {
+      setConnected(socket.connected);
+    });
+  }, [socket]);
+
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      {isConnected ? 'Connected' : 'Disconnected'}
+    </div>
   )
 }
 
